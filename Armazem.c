4da90b40c7a57cf9ce13e7f37bda20a55b62fe76
Armando Magalhães <armando.mag95@gmail.com>
2017-12-04 13:56:00 -0200

    simple refactor

--- Armazem.c
+++ Armazem.c
@@ -4,14 +4,26 @@ int main()
 {
 
     /** preço de 10 produtos armazenados em um vetor */
+
+    /**
+    * TODO: Descomentar antes de entregar
+    */ 
     // float produtos[10];
     float produtos[10] = {1,2,3,4,5,6,7,8,9,10};
 
     /** código de 5 armazéns */
+
+    /**
+    * TODO: Descomentar antes de entregar
+    */ 
     // int arm[5];
     int arm[5] = {43,53,2132,32,111};
     
     /** quantidade estocada em cada um desses produtos em cinco armazéns diferentes */
+
+    /**
+    * TODO: Descomentar antes de entregar
+    */ 
     // int mat[5][10];
     int mat[5][10] = {
         {1,2,3,4,5,6,7,8,9,10},
@@ -23,7 +35,6 @@ int main()
 
     //E) 
     int listaCustoArm[5]; 
-
 
     /** soma dos produtos */
     float somaP = 0;
@@ -48,6 +59,9 @@ int main()
     /** recebendo os valores dos produtos */
     for(int i=0;i<10;i++)
     {
+        /**
+        * TODO: Descomentar antes de entregar
+        */ 
         // printf("Informe o valor do produto %i: ", i);
         // scanf("%f", &produtos[i]);
     }
@@ -57,6 +71,9 @@ int main()
     /** recebendo os valores dos armazéns */
     for(int i=0;i<5;i++)
     {
+        /**
+        * TODO: Descomentar antes de entregar
+        */ 
         // printf("Informe o código do armazém %i: ", i);
         // scanf("%d", &arm[i]);
     }
@@ -67,17 +84,22 @@ int main()
     for(int i=0;i<5;i++)
     {
         int somaProdutoE = 0;
+
         for(int j=0;j<10;j++)
         {
+            /**
+             * TODO: Descomentar antes de entregar
+             */ 
             // printf("Informe a quantidade do produto %i no armazem %i: ", j, i);
             // scanf("%d", &mat[i][j]);
 
             /** B) somando */
             somaP += mat[i][j];
+
             //D
             somaProdutoE += mat[i][j];
             somaProdutoE += mat[i][j];
-            listaCustoArm[i]+=produtos[j]*mat[i][j];
+            listaCustoArm[i] += produtos[j] * mat[i][j];
 
             /** C) maior estoque */
             if(maiorE < mat[i][j])
@@ -86,10 +108,12 @@ int main()
                 produtoMaiorEstoque = j;
             }
         }
+
         if(menorE > somaProdutoE)
         {
-            menorE = somaProdutoE ;
+            menorE = somaProdutoE;
         }
+
         vetSomaProd[i] = somaProdutoE;
     }
 
@@ -122,17 +146,14 @@ int main()
             printf("\nCódigo do Armazem: %i", arm[i]);
             printf("\nSoma de Produtos: %i", listaCustoArm[i]);
         }
-
     }
+
     // D)
     for(int i=0; i<5; i++)
     {
-    
         printf("\n\nCódigo do Armazem: %i", arm[i]);
         printf("\nCusto do armazem: %i", listaCustoArm[i]);
-
     }
-    scanf("")
 
     return 0;
 }
--- README.md
+++ README.md
@@ -20,9 +20,15 @@ Elabore um programa em C que receba:
 O programa deverá calcualr e imprimir:
 
  a) a quantidade de produtos estocados em cada um dos armazéns;
+
  b) a quantidade de cada um dos produtos estocados em todos os armazéns juntos;
+ 
  c) o preço do produto que possui maior estoque em um único armazém;
- d) o menor estoque armazenado. Imprimir o código do armazém que contém o menor estoque armazenado e o menor estoque armazenado. Supor que pode haver mais de um armazém com o menor estoque armazenado;
+ 
+ d) o menor estoque armazenado. Imprimir o código do armazém que contém o menor estoque armazenado e o menor estoque 
+ armazenado. Supor que pode haver mais de um armazém com o menor estoque armazenado;
+ 
  e) o custo de cada armazém. Imprimir o código de cada armazém e seu respectivo custo total armazenado.
+ 
  f) a quantidade de produtos com preço entre R$ 50,00 e R$ 100,00 (inclusive) que tem a quantidade em estoque maior que 2000 em determinado armazém, cujo código é digitado pelo usuário. O usuário poderá digitar um código não cadastrado; neste caso, o programa deverá mostrar uma mensagem de erro.
 
